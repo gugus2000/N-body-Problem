5ecc5151aada4a54b27f7edb381356825a9db518
gugus2000 <gugus2000@protonmail.com>
2021-01-11 13:02:52 +0100

    Clean & display

--- viewstorage.py
+++ viewstorage.py
@@ -6,4 +6,10 @@ C_FILE='SAVE.npy'
 C_FILE=input('Enter the name of the file you want to display')
 
 with open(C_FILE, 'rb') as save:
-    print(np.load(save))
+    bodies=np.load(save, allow_pickle=True)
+
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+for body in bodies:
+    plt.plot([body.trajectory[date][0] for date in range(len(body.trajectory))], [body.trajectory[date][1] for date in range(len(body.trajectory))])
+plt.show()
